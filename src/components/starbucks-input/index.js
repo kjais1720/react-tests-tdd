1c1f4f6b50c792d8da96995e70bc9c4f8d430240
Krituraj Anand <jaiswalkrituraj20@gmail.com>
2022-12-06 16:46:58 +0530

    fix: satisfy 3rd test

--- src/components/starbucks-input/index.js
+++ src/components/starbucks-input/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 const StarbucksInput = ({name, value, onChange, error}) => {
   return (
     <>
+    <span>{error}</span>
     <label htmlFor={name}>{name}</label>
     <input id={name} value={value} onChange={e=> onChange(e.target.value)} />
     </>
